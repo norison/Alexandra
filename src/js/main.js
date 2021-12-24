b0f5f7d2c9ab737263e5007e4934b914f39cb9ce
Ihor Matiev <ihor.matiev@gmail.com>
2021-12-25 00:59:33 +0200

    added part of skills section

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const close = document.querySelector(".menu__close");
   const overlay = document.querySelector(".menu__overlay");
   const menu = document.querySelector(".menu");
+  const menuLinks = document.querySelectorAll(".menu__link");
 
   burger.addEventListener("click", () => {
     menu.classList.add("menu_active");
@@ -14,5 +15,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
   overlay.addEventListener("click", () => {
     menu.classList.remove("menu_active");
+  });
+
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("menu_active");
+    });
   });
 });
